1cf761c2877031af4d2b3ab5e89d326ecf627729
Sumit Kumar <sumitbatwani@gmail.com>
2022-09-20 10:19:13 +0530

    testing git commit

--- print.py
+++ print.py
@@ -1,16 +1,16 @@
-print(3 + 4) # = 7
+print(3 + 4) # 7
 
-print(2 - 3) # = -1
+print(2 - 3) # -1
 
-print(2 * 3) # = 6
+print(2 * 3) # 6
 
-print(2 / 4) # = 0.5
+print(2 / 4) # 0.5
 
 # Floor Division
-print(9 // 4) # = 2
+print(9 // 4) # 2
 
 # Modulus
-print(9 % 4) # = 1
+print(9 % 4) # 1
 
 # Exponentiation
-print(3 ** 4) # = 81
+print(3 ** 4) # 81
