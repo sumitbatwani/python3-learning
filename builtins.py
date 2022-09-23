d19ea57fb20da37abbf91e28a76c52f812c18522
Sumit Kumar <sumitbatwani@gmail.com>
2022-09-23 11:17:10 +0530

    dictionary, tuples and builtins

---
+++ builtins.py
@@ -0,0 +1,7 @@
+student_grades = [9.1, 8.8, 7.5]
+# dir(__builtins__) to check for built in functions
+# help(function_name) to learn to execute
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum/length
+print(mean)
